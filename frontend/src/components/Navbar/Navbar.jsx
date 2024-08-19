a7c33da970332117325b7af5340184051fdd1ae6
kunal-216 <kunaltalreja216@gmail.com>
2024-08-20 04:49:06 +0530

    first commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
 
-  const [menu,setMenu] = useState('home');
+  const [menu, setMenu] = useState('home');
   const token = true;
 
   return (
@@ -13,17 +13,23 @@ const Navbar = () => {
         <Link to="/"><img src="" alt="Logo" /></Link>
       </div>
       <div className='flex space-x-6'>
-        <Link to="/" onClick={()=>setMenu("Home")} className={menu==="Home"? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Home</Link>
-        <Link to="/jobs" onClick={()=>setMenu("Jobs")} className={menu==="Jobs"? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Jobs</Link>
-        <Link to="/internships" onClick={()=>setMenu("Internships")} className={menu==="Internships"? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Internships</Link>
-        <Link to="/connect" onClick={()=>setMenu("Connect")} className={menu==="Connect"? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Connect</Link>
-        <Link to="/contact" onClick={()=>setMenu("Contact")} className={menu==="Contact"? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Contact Us</Link>
+        <Link to="/" onClick={() => setMenu("Home")} className={menu === "Home" ? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Home</Link>
+        <Link to="/jobs" onClick={() => setMenu("Jobs")} className={menu === "Jobs" ? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Jobs</Link>
+        <Link to="/internships" onClick={() => setMenu("Internships")} className={menu === "Internships" ? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Internships</Link>
+        <Link to="/connect" onClick={() => setMenu("Connect")} className={menu === "Connect" ? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Connect</Link>
+        <Link to="/contact" onClick={() => setMenu("Contact")} className={menu === "Contact" ? 'hover:text-gray-400 underline underline-offset-4 decoration-blue-200' : 'hover:text-gray-400'}>Contact Us</Link>
       </div>
       <div className='flex space-x-4'>
-        <Link to="/dashboard" className='flex items-center'>
-          <FaUserCircle className='w-10 h-10 rounded-full' />
-        </Link>
-        {token ? <Link to="/user" onClick={()=>setMenu("Sign Up")} className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded'>Sign Up</Link> : <></>}
+        {token ?
+          <>
+            <Link to="/dashboard" className='flex items-center'>
+              <FaUserCircle className='w-10 h-10 rounded-full' />
+            </Link>
+            <Link to="/user" onClick={() => setMenu("Sign Up")} className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded'>Sign Up</Link>
+          </>
+          : 
+          <>
+          </>}
       </div>
     </nav>
   )
