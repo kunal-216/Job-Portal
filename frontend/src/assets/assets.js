c9d284b0013396b53f2c448c55a6aa453f2c703e
kunal-216 <kunaltalreja216@gmail.com>
2024-08-27 04:24:31 +0530

    first commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import heroImg from './heroimg.jpg'
+import heroImg from './heroImg.jpg'
 import logo from './logo.png'
 import favicon from "./favicon.png"
 import upload_area from "./upload_area.png"
