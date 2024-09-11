d68082a305639d31ec57a4b1db967b2a50109d22
kunal-216 <kunaltalreja216@gmail.com>
2024-09-12 03:03:34 +0530

    some error fixed

--- backend/server.js
+++ backend/server.js
@@ -47,12 +47,6 @@ app.use((req, res, next) => {
     res.status(404).send("Sorry, can't find that!");
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
